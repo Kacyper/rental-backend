0720ac097913afe3705a04887c1b0da9cd0a293f
Kacper <kksykiewicz@gmail.com>
2022-05-16 16:30:39 +0200

    first commit

---
+++ src/test/java/com/kacyper/carrentalbackend/CarRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacyper.carrentalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
