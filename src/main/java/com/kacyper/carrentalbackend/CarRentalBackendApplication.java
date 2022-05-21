20049661c102bc1206409b152b1023aae0d0823c
Kacper <kksykiewicz@gmail.com>
2022-05-21 14:18:42 +0200

    application properties

--- src/main/java/com/kacyper/carrentalbackend/CarRentalBackendApplication.java
+++ src/main/java/com/kacyper/carrentalbackend/CarRentalBackendApplication.java
@@ -1,9 +1,11 @@
 package com.kacyper.carrentalbackend;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class CarRentalBackendApplication {
 
     public static void main(String[] args) {
